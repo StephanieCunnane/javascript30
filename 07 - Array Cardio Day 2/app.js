20c29e20f6a64fbdc1ce4b68faf85519cc465cbc
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-05-10 11:18:57 -0700

    Comment out answers to exercises

--- 07 - Array Cardio Day 2/app.js
+++ 07 - Array Cardio Day 2/app.js
@@ -15,36 +15,36 @@ const comments = [
 
 
 
-// 1. Is at least one person an adult? (18 or over)
-// Use Array.prototype.some()
-const currentYear = (new Date).getFullYear();
-const includesAdult = people.some(person => currentYear - person.year >= 18);
-console.log('includesAdult:', includesAdult);
+// // 1. Is at least one person an adult? (18 or over)
+// // Use Array.prototype.some()
+// const currentYear = (new Date).getFullYear();
+// const includesAdult = people.some(person => currentYear - person.year >= 18);
+// console.log('includesAdult:', includesAdult);
 
 
 
-// 2. Is everyone an adult? (18 or over)
-// Use Array.prototype.every()
-const allAdults = people.every(person => currentYear - person.year >= 18);
-console.log('allAdults:', allAdults);
+// // 2. Is everyone an adult? (18 or over)
+// // Use Array.prototype.every()
+// const allAdults = people.every(person => currentYear - person.year >= 18);
+// console.log('allAdults:', allAdults);
 
 
 
-// 3. Find the comment with an ID of 823423
-// Array.prototype.find()
-const commentFound = comments.find(comment => comment.id === 823423);
-console.log('commentFound:', commentFound);
+// // 3. Find the comment with an ID of 823423
+// // Array.prototype.find()
+// const commentFound = comments.find(comment => comment.id === 823423);
+// console.log('commentFound:', commentFound);
 
 
 
-// 4. Delete the comment with an ID of 823423 
-// Array.prototype.findIndex()
-const commentToDelete = comments.findIndex(comment => comment.id === 823423);
-console.log('commentToDelete:', commentToDelete);
+// // 4. Delete the comment with an ID of 823423 
+// // Array.prototype.findIndex()
+// const commentToDelete = comments.findIndex(comment => comment.id === 823423);
+// console.log('commentToDelete:', commentToDelete);
 
-comments.splice(commentToDelete, 1);
-// Use console.table for readability
-console.table(comments);
+// comments.splice(commentToDelete, 1);
+// // Use console.table for readability
+// console.table(comments);
 
 
 
